c1aa72a9c126280096a3de6afe95b6aa5fe2d322
shin.liu <shin820@163.com>
2017-02-05 18:21:18 +0800

    spike angular2

---
+++ AngularJs2Spike/angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+/*
+当 TypeScript 看到@Injectable()装饰器时，就会生成本服务的元数据。如果 Angular 需要往这个服务中注入其它依赖，就会使用这些元数据。
+作为一项最佳实践，无论是出于提高统一性还是减少变更的目的， 都应该从一开始就加上@Injectable()装饰器。
+ */
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+    
+    getHeroesSlowly():Promise<Hero[]>{
+        return new Promise<Hero[]>(reslove=>setTimeout(reslove,2000))
+        .then(()=>this.getHeroes());
+    }
+}
