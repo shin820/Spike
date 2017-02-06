fe53ad4451aeb14d9dda2826875e6d07fab03eb1
shin.liu <shin820@163.com>
2017-02-06 16:22:56 +0800

    update

--- AngularJs2Spike/angular-tour-of-heroes/src/app/dashboard.component.ts
+++ AngularJs2Spike/angular-tour-of-heroes/src/app/dashboard.component.ts
@@ -11,7 +11,10 @@ import { HeroService } from './hero.service'
 export class DashboardComponent implements OnInit {
     
     heroes:Hero[]=[];
-
+    /*
+    组件类应保持精简。组件本身不从服务器获得数据、不进行验证输入，也不直接往控制台写日志。 它们把这些任务委托给服务。
+    组件的任务就是提供用户体验，仅此而已。
+     */
     constructor(private heroService:HeroService) { }
 
     ngOnInit() { 
