1efa680fc3348bf4f65294a47f8b10947eb4e043
shin.liu <shin820@163.com>
2017-02-22 15:48:49 +0800

    add ef core.

---
+++ DotNetCoreSpike/DotNetCoreSpike.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DotNetCoreSpike.IdentityServer.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
--- DotNetCoreSpike/DotNetCoreSpike.IdentityServer/Startup.cs
+++ DotNetCoreSpike/DotNetCoreSpike.IdentityServer/Startup.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreSpike.IdentityServer.Data;
+using DotNetCoreSpike.IdentityServer.Models;
 using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using IdentityServer4.EntityFramework.Mappers;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
 
 namespace DotNetCoreSpike.IdentityServer
 {
@@ -29,15 +36,25 @@ namespace DotNetCoreSpike.IdentityServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
+
             // Add framework services.
             services.AddMvc();
 
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddIdentityServer()
                 .AddTemporarySigningCredential()
-                .AddInMemoryIdentityResources(Config.GetIdentityResources()) //FOR OIDC
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers()); // For Resource Owner
+                .AddTestUsers(Config.GetUsers())
+                .AddConfigurationStore(builder => builder.UseMySql(Configuration.GetConnectionString("DefaultConnection"), options =>
+                options.MigrationsAssembly(migrationsAssembly)))
+                .AddOperationalStore(builder => builder.UseMySql(Configuration.GetConnectionString("DefaultConnection"), options =>
+                options.MigrationsAssembly(migrationsAssembly)))
+/*                .AddAspNetIdentity<ApplicationUser>()*/;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +62,8 @@ namespace DotNetCoreSpike.IdentityServer
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            //app.UseIdentity();
 
             app.UseIdentityServer();
 
@@ -87,6 +106,45 @@ namespace DotNetCoreSpike.IdentityServer
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+
+            InitializeDatabase(app);
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
